803389d695d23bf51a619be0c4836f561096d257
HarelYehiel <hyehiel@g.jct.ac.il>
2021-10-06 11:51:12 +0300

    I add 2 functions.

---
+++ Targil0/Program4259.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome4259();
+            Wellcome6393();
+
+            Console.ReadKey();
+
+        }
+
+        static partial void Wellcome6393();
+
+        private static void Wellcome4259()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
