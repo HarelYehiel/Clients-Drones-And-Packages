1e86de08f2ca5e49282b7b8f92a38bb0e6561756
יהונתן טי <ythee@g.jct.ac.il>
2021-10-11 09:04:43 +0300

    I created all the basic struct' we need to input the properties by the Targil

---
+++ ClassLibrary1/Drone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {//all the properties
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight{ get; set; }
+            public DroneStatus Status{ get; set; }
+            public double Battery { get; set; };
+
+            public override string ToString()
+            {
+                return $"Drone ID: {Id}, model: {Model}, status: {Status}, maxWeight: {MaxWeight}, battery: {Battery}";
+            }            
+        }
+        public struct WeightCategories
+        {
+            //I am not sure that this is the way to do it
+        }
+        public struct DroneStatus
+        {
+            //I am not sure that this is the way to do it
+        }
+
+    }
+}
