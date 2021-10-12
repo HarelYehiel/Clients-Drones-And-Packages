2321a4d6026def51d470f6912e93303338845165
יהונתן טי <ythee@g.jct.ac.il>
2021-10-12 12:24:50 +0300

    I started to work on Data source.. will take time

--- ClassLibrary1/DataSource.cs
+++ ClassLibrary1/DataSource.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace IDAL
 {
-    namespace DO
+    namespace DalObject
     {
-        public struct DataSource
+        public class DataSource
         {
+            public DO.Drone[] droens = new DO.Drone[10];
+            public DO.station[] stations = new DO.station[5];
+            public DO.Customer[] customers = new DO.Customer[100];
+            public DO.Parcel[] parcels = new DO.Parcel[1000];
+            
+            public static void Initialize()
+            {
+            
+            }
+        }
+        public struct Cofing
+            {
+                static DalObject.DataSource.
         }
     }
 }
