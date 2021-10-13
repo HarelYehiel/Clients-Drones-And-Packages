3476990f2228854f4ae3a13c13a940a334fc7646
יהונתן טי <ythee@g.jct.ac.il>
2021-10-13 10:27:25 +0300

    try

--- ClassLibrary1/DataSource.cs
+++ ClassLibrary1/DataSource.cs
@@ -9,23 +9,26 @@ namespace IDAL
 {
     namespace DalObject
     {
-        public class DataSource
+        public struct DataSource
         {
-            private static DO.Drone[] droens = new DO.Drone[10];
+            public static DO.Drone[] drones = new DO.Drone[10];
             public static DO.station[] stations= new DO.station[5];
             public static DO.Customer[] customers = new DO.Customer[100];
             public static DO.Parcel[] parcels = new DO.Parcel[1000];
-
-           
-
-            public static void Initialize()
+            internal struct Cofing
             {
-            
+                public static int droneIndex = 0;
+                public static int stationIndex =0;
+                public static int customersIndex = 0;
+                public static int parcelsIndex = 0;
+                
+            }
+
+            public static void Initialize() 
+            {
+
             }
         }
-        public struct Cofing
-            {
-            
-        }
+        
     }
 }
