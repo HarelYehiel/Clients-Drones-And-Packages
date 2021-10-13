2b615f0e89c49ecb0124a9219cda09b94a324597
יהונתן טי <ythee@g.jct.ac.il>
2021-10-13 23:08:25 +0300

    I finished the all properties and method and also I changed small things at all the classes(YESHUIOT) update it in your project and the next levl is the main

--- ClassLibrary1/DataSource.cs
+++ ClassLibrary1/DataSource.cs
@@ -32,8 +32,8 @@ namespace IDAL
                     var rand = new Random();
                     drones[i].Id = rand.Next(10000, 99999);
                     drones[i].Battery = rand.Next(1, 100);
-                    //drones[i].MaxWeight = rand.Next(WeightCategories)
-                    //drones[i].Model = 
+                    drones[i].MaxWeight = (WeightCategories)rand.Next(0, 2);
+                    drones[i].Status = (DroneStatus)rand.Next(0,2);
                     Cofing.droneIndex++;//elert the index of the free cells at drones arry
                 }
                 for (int i = 0; i < 2; i++)//crate 2 staitons with random data
@@ -42,9 +42,8 @@ namespace IDAL
                     var rand = new Random();
                     stations[i].Id = rand.Next(10000,99999);
                     stations[i].name = "sta" + rand.Next(1, 99);
-                    //stations[i].MaxWeight = 
-                    //stations[i].latitude = 
-                    //stations[i].longitude = 
+                    stations[i].latitude = 31 + rand.Next(0, 1);
+                    stations[i].longitude = 34 + rand.Next(0, 1);
                     Cofing.stationIndex++; //elert the index of the free cells at staions arrry
                 }
                 for(int i = 0; i < 10; i++)//crate new 10 random coustomers
@@ -54,8 +53,8 @@ namespace IDAL
                     customers[i].Id = rand.Next(11111, 99999);
                     customers[i].Name = "cust" + rand.Next(1, 99);
                     customers[i].Phone = "05" + rand.Next(10000000, 99999999);
-                    //customers[i].latitude =
-                    //customers[i].longitude = 
+                    customers[i].latitude = 31 + rand.Next(0, 1);
+                    customers[i].longitude = 34 + rand.Next(0, 1);
                     Cofing.customersIndex++;//elert the index of the free cell at customers array
                 }
                 for(int i=0;i<10; i++)//crate new 10  parcels with random data
@@ -66,12 +65,13 @@ namespace IDAL
                     parcels[i].SenderId = rand.Next(11111, 99999);
                     parcels[i].TargetId = rand.Next(11111, 99999);
                     parcels[i].DroneId = rand.Next(11111, 99999);
-                    //parcels[i].Weight =
-                    //parcels[i].Priority = 
-                    //parcels[i].Requested = 
-                    //parcels[i].Scheduled = 
-                    //parcels[i].PickedUp = 
-                    //parcels[i].Delivered = 
+                    parcels[i].Weight = (WeightCategories)rand.Next(0, 2);
+                    parcels[i].Priority = (Parcel.Priorities)rand.Next(0, 2);
+                    DateTime start = new DateTime(2021, rand.Next(1, 12), rand.Next(1, 31));//crate random time and colculate all the next properties
+                    parcels[i].Requested = start.AddMinutes(rand.Next(1, 240));
+                    parcels[i].Scheduled = parcels[i].Requested.AddMinutes(rand.Next(20, 60));
+                    parcels[i].PickedUp = parcels[i].Requested.AddMinutes(rand.Next(5, 20));
+                    parcels[i].Delivered = parcels[i].Requested.AddMinutes(rand.Next(30, 90));
                     Cofing.parcelsIndex++;//elert the index of the free cell at parcels array
                 }
             }
