9de238f3b527d57de39e094bffd248ad83c501cf
HarelYehiel <hyehiel@g.jct.ac.il>
2021-10-13 12:56:56 +0300

    I insert int DalObject: * Functions that found and return the place to id (ParcelId, CustomerId, droneId, stationId) in the  array.
    * Add function that return the new parcel-Id for new Parce.

--- ClassLibrary1/Parcel.cs
+++ ClassLibrary1/Parcel.cs
@@ -10,6 +10,10 @@ namespace IDAL
     {
         public struct Parcel
         {
+            public Parcel(int id, int y, int t, int r) : this()
+            {
+                
+            }
             public int Id{get; set;}
             public int SenderId{ get; set;}
             public int TargetId{get; set;}
