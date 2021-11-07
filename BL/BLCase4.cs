cbfc3dfd20d94f06c0c9d5bd3b47135b39220782
HarelYehiel <hyehiel@g.jct.ac.il>
2021-11-07 15:25:49 +0200

    Add MyException. Add the bodies in case 4

--- BL/BLCase4.cs
+++ BL/BLCase4.cs
@@ -10,44 +10,73 @@ namespace IBL
 {
     public partial class BL : IBL
     {
-        //public void List_view_options() { } לא יודע מה זה
 
         /* In function List_view_options*/
         public List<station> Displays_a_list_of_base_stations()
         {
+            if (DataSource.stations.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
+
             List<station> stations_1 = new List<station>();
             stations_1 = DataSource.stations.ToList<station>();
             return stations_1;
         }
         public List<Drone> Displays_the_list_of_drones()
         {
+            if (DataSource.drones.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
+
             List<Drone> drones_1 = new List<Drone>();
             drones_1 = DataSource.drones.ToList<Drone>();
             return drones_1;
         }
         public List<Customer> Displays_a_list_of_customers()
         {
+            if (DataSource.customers.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
+
             List<Customer> Customer_1 = new List<Customer>();
             Customer_1 = DataSource.customers.ToList<Customer>();
             return Customer_1;
         }
         public List<Parcel> Displays_the_list_of_Parcels()
         {
+            if (DataSource.parcels.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
+
             List<Parcel> parcels_1 = new List<Parcel>();
             parcels_1 = DataSource.parcels.ToList<Parcel>();
             return parcels_1;
         }
-        public void Displays_a_list_of_Parcels_not_yet_associated_with_the_drone()
+        public List<Parcel> Displays_a_list_of_Parcels_not_yet_associated_with_the_drone()
         {
+            if (DataSource.parcels.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
 
             List<Parcel> parcels_1 = new List<Parcel>();
-
             foreach (Parcel item in DataSource.parcels)
             {
-                if (item.Scheduled == DateTime.MinValue)  ???????????????/
+                if (item.DroneId == 0)
+                    // Not associated drone to parcel. 
+                    parcels_1.Add(item);
             }
+            return parcels_1;
         }
-        public void Display_of_base_stations_with_available_charging_stations() { }
+        public List<station> Display_of_base_stations_with_available_charging_stations() {
+
+            if (DataSource.stations.Count == 0)
+                throw new MyExeption(MyExeption.An_empty_list);
+                
+            List<station> stations_1 = new List<station>();
+            foreach (station item in DataSource.stations)
+            {
+                if (item.ChargeSlots > 0)
+                    // Available place to charging.
+                    stations_1.Add(item);
+            }
+
+            return stations_1;
+        }
         /* Until here */
     }
 }
