c488c55e5c13edd0504a5a6841324ed2acb47fba
HarelYehiel <hyehiel@g.jct.ac.il>
2021-11-25 12:39:22 +0200

    Fix case 2.

--- BL/BLCase2.cs
+++ BL/BLCase2.cs
@@ -65,7 +65,7 @@ namespace IBL
                 if (phoneNumber != "")
                     customer.phone = phoneNumber;
                 updateDataSourceFun.updateCustomer(customer);
-            }
+            }   
             catch (Exception e)
             {
 
@@ -118,7 +118,7 @@ namespace IBL
                         IDAL.DO.Station sta = new IDAL.DO.Station();
                         foreach (var station in IDAL.DalObject.DataSource.stations)
                         {
-                            if (station.Location.latitude == point2.latitude && station.Location.longitude == point2.longitude )
+                            if (station.Location.latitude == point2.latitude && station.Location.longitude == point2.longitude)
                             {
                                 if (station.ChargeSlots > 0)
                                     sta = station;
