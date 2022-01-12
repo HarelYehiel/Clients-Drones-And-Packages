32a6d8be78ed0a68817627b6819d2d9baf8f0851
HarelYehiel <hyehiel@g.jct.ac.il>
2022-01-12 21:55:25 +0200

    marge

--- PL/pages/DroneListPage.xaml.cs
+++ PL/pages/DroneListPage.xaml.cs
@@ -1,24 +1,11 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using BO;
-using System.Runtime.InteropServices;
-using System.Threading;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-using System.Windows.Interop;
 
 
 namespace PL
